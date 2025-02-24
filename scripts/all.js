1f73260beceffd957c451b8813aef3235dfb2ee9
krisBG51 <krisbg51@gmail.com>
2025-02-24 09:02:08 +0200

    7

--- scripts/all.js
+++ scripts/all.js
@@ -22,9 +22,20 @@ function displayMovies(movies) {
 
     movies.forEach(movie => {
         let movieCard = document.createElement('div');
-        movieCard.classList.add('flip', 'flip-vertical')
-        movieCard.innerHTML = '
-           
-        ' 
-    })
+        movieCard.classList.add('flip', 'flip-vertical');
+        movieCard.innerHTML = `
+            <div class = "card">
+                <div class = "front">
+                    <img src = "https://image.tmdb.org/t/p/w500${movie.poster_path}" alt = "${movie.title}">
+                </div>
+                <div class = "back">
+                    <h2>${movie.title}</h2>
+                    <p>Description: ${movie.overview}</p>
+                    <p>Vote Average: ${movie.vote_average}</p>
+                </div>
+            </div>
+
+        `;
+        movieContainer.appendChild(movieCard);   
+    })  
 }
